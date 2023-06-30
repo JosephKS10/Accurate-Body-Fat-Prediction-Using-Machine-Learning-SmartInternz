1633079553f902cb1ece162f2d6992f3ddb2711d
Joseph K Saji <jjbrothersdelhi@hotmail.com>
2023-06-30 15:10:42 +0530

    Project Report

--- app/app.py
+++ app/app.py
@@ -9,7 +9,8 @@ model = joblib.load('model.pkl')
 # Route for rendering the HTML form
 @app.route('/')
 def home():
-    return render_template('index.html',check=False)
+    age = 0;
+    return render_template('index.html',check=False, age=age)
 
 # Route for handling form submission
 @app.route('/predict', methods=['POST'])
